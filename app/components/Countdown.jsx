203ec8ea48ab61e53a7b1fe0bc2122a8a0153bfb
Jatin Suneja <jatinsuneja1903@gmail.com>
2017-10-07 00:15:27 -0700

    Add countdown functionality

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -5,12 +5,36 @@ var CountdownForm = require ('CountdownForm');
 var Countdown = React.createClass ({
 
     getInitialState: function () {
-        return {count : 0};
+        return {
+        count : 0,
+        countdownStatus: 'stopped' // the state of the countdown wether it is started or paused or stopped
+        };
     },
+
+    componentDidUpdate: function (prevProps, prevState) {
+        if(this.state.countdownStatus !== prevState.countdownStatus) { // matches if the new countdown status which updates every second is still equal to the previous countdown status or not
+            switch (this.state.countdownStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+            }
+        }
+    },
+
+    startTimer: function () {
+        this.timer = setInterval(() => { // set the interval 
+            var newCount = this.state.count -1;  // calculate the new count
+            this.setState({
+                count: newCount >=0 ? newCount: 0 // set the new state // it also checks if the 'newCount' is less than 0 or not !, if not then it keeps on running but if it is than it sets newCount: 0
+            })
+        }, 1000);  // provided the time it requires 
+    },
+
 
     handleSetCountdown: function (seconds) {
         this.setState ({
-            count: seconds
+            count: seconds,
+            countdownStatus: 'started' // countdown status is started
         });
     },
 
--- app/components/TImer.jsx
+++ app/components/TImer.jsx
@@ -1,4 +1,5 @@
 var React = require ('react');
+var Clock = require ('clock');
 
 var Timer = React.createClass ({
     render: function () {
